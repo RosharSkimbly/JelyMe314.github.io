31507bf552b49b858d8f5835dfc2a4d55ffbab46
JelyMe <21324@nayland.school.nz>
2024-07-31 16:27:00 +1200

    First Iteration

---
+++ index.js
@@ -0,0 +1,6 @@
+function MenuClick(){
+    document.getElementById("main-content").style = "height:70vh; opacity:1;";
+}
+function CloseMainContent(){
+    document.getElementById("main-content").style = "height:0vh;";
+}
