5f51a12b4f575e0b65761a6f248e4a0e7dd2f334
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 <21324@nayland.school.nz>
2024-08-01 10:05:17 +1200

    Smooth moves

--- mobile.js
+++ mobile.js
@@ -1,18 +1,31 @@
+isOpen = false
+
+previousText = ""
+
 function MenuClick(index, object){
-    content = document.getElementById("main-content");
-    parent = content.parentNode;
-    parent.childNodes.forEach(node => {
-        node.style = "opacity: 0; height:0vh;";
-    });
-    object.style = "";
-    parent.insertBefore(content, object);
-    setTimeout(()=>{
-        content.style = "height:70vh; opacity:1;";
-    },10);
+    if(isOpen){
+        object.innerHTML = previousText;
+        CloseMainContent();
+    }else{
+        content = document.getElementById("main-content");
+        parent = content.parentNode;
+        parent.childNodes.forEach(node => {
+            node.style = "opacity: 0; height:0vh; margin:0;";
+        });
+        object.style = "";
+        previousText = object.innerHTML;
+        object.innerHTML = " ".repeat(previousText.length/2) + "X" + " ".repeat(previousText.length/2);
+        parent.insertBefore(content, object);
+        setTimeout(()=>{
+            content.style = "height:70vh; opacity:1;";
+            isOpen = true;
+        },10);
+    }
 }
 function CloseMainContent(){
     parent.childNodes.forEach(node => {
         node.style = "";
     });
     document.getElementById("main-content").style = "height:0vh;";
+    isOpen = false;
 }
